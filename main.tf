4e55303e2c1ee01405aaba277ca192eeabc289bc
Naveen-Gandham-07 <naveengandham0706@gmail.com>
2026-05-08 21:27:30 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t2.micro"
+    ami = "ami-0f559c3642608c138"
+    instance_type = "t3.micro"
     tags = {
       Name = "aws-instance-4"
     }
